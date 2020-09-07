8be0b0b58bdab29127b187af0b2d475e89acc14f
DatCodero <chipcop106@gmail.com>
2020-09-07 18:20:50 +0700

    Route home demo

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import Layout from '~/components/Layout'
 import { Button, Box } from '@material-ui/core'
+import Home from '~/pages/home'
 const Authentication = () => {
-	return (
-		<Layout>
-			<Box bgcolor="secondary.main">
-				<Button variant="contained" color="primary">
-					Primary
-				</Button>
-			</Box>
-		</Layout>
-	)
+	return <Home />
 }
 
 export default Authentication
